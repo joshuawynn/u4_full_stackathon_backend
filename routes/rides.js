696dbc8479523e90582820a07dae745129de5ca0
Rachel Carleton <rachel.carleton@icloud.com>
2024-02-05 10:14:11 -0800

    routes update

--- routes/rides.js
+++ routes/rides.js
@@ -9,5 +9,18 @@ router.post("/", async (req, res) =>  {
 	res.status(201).json({message: "rides create route"})
 });
 
+router.get("/:id", (req, res) => {
+	res.status(200).json({message: "rides show route: " + req.params.id })
+});
+
+router.delete("/:id", (req, res) => {
+	res.status(200).json({message: "rides delete route: " + req.params.id })
+});
+
+router.put("/:id", (req, res) => {
+	console.log(req.body)
+	res.status(200).json({message: "rides update route: " + req.params.id })
+});
+
 module.exports = router
 
